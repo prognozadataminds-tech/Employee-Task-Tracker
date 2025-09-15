ef9f0373ebf0ea02ad16f43348410ee4fde767ef
vignesh <vigneshchinnadurai467@gmail.com>
2025-09-15 12:13:10 +0530

    allotmentid chg stg to num

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -5,7 +5,7 @@ const taskSchema = new mongoose.Schema(
     employeeName: { type: String, required: true },
     task: { type: String, required: true },
     total: { type: Number, required: true },
-    allotmentID: { type: String },
+    allotmentID: { type: Number },
     completed: { type: Number, required: true },
      count: { type: Number, default: 0 },
     pending: { type: Number, required: true },
